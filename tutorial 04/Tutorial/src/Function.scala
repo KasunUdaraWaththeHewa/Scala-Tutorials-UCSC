c3f5c40681f5c1a710fcd51f881c72628e6a4f74
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-07-03 20:16:18 +0530

    new commit

---
+++ tutorial 04/Tutorial/src/Function.scala
@@ -0,0 +1,20 @@
+object Function {
+  def toUpper(input: String): String = {
+    input.toUpperCase()
+  }
+
+  def toLower(input: String): String = {
+    input.toLowerCase()
+  }
+
+  def formatNames(name: String, formatter: String => String): String = {
+    formatter(name)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(formatNames("Benny", toUpper));
+    println(formatNames("Ni", toUpper)+formatNames("roshan", toLower));
+    println(formatNames("Saman", toLower));
+    println(formatNames("K", toUpper)+formatNames("umar",toLower)+formatNames("a",toUpper));
+  }
+}
