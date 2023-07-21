60bae67518a2366535eeaa94a5dd4007dca42cc1
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-07-21 13:27:18 +0530

    new commit

--- tutorial 05/Tutorial/src/Fibonacci.scala
+++ tutorial 05/Tutorial/src/Fibonacci.scala
@@ -2,13 +2,10 @@ object Fibonacci {
   def fibonacci(n: Int): List[Int] = {
     if (n <= 0)
       return List()
-
     if (n == 1)
       return List(0)
-
     if (n == 2)
       return List(0, 1)
-
     val fibList = fibonacci(n - 1)
     val nextNumber = fibList(n - 2) + fibList(n - 3)
     return fibList :+ nextNumber
