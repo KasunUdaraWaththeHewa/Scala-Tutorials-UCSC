0766a9c2131b3043caf905aac7cb2f19fe43688c
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-08-07 19:12:08 +0530

    Initiallized the git

---
+++ tutorial 07/untitled/src/FilterEvenNumbers.scala
@@ -0,0 +1,11 @@
+object FilterEvenNumbers {
+  def filterEvenNumbers(numbers: List[Int]): List[Int] = {
+    numbers.filter(number => number % 2 == 0);
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    val output = filterEvenNumbers(input);
+    println(output);
+  }
+}
