56fd165ff2010d57b786ab30efc64d65be9f6997
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-08-07 19:15:46 +0530

    new commit

--- tutorial 07/untitled/src/FilterEvenNumbers.scala
+++ tutorial 07/untitled/src/FilterEvenNumbers.scala
@@ -4,7 +4,7 @@ object FilterEvenNumbers {
   }
 
   def main(args: Array[String]): Unit = {
-    val input =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    val input = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     val output = filterEvenNumbers(input);
     println(output);
   }
