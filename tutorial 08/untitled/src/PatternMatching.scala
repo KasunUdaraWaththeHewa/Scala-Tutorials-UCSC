bf68ea2f71d100699001f842b0e477aba1305e6f
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-08-12 19:11:48 +0530

    Second Problem Done

--- tutorial 08/untitled/src/PatternMatching.scala
+++ tutorial 08/untitled/src/PatternMatching.scala
@@ -1,5 +1,12 @@
 object PatternMatching {
   def main(args: Array[String]): Unit = {
-
+    println("Please enter an integer:")
+    val input = scala.io.StdIn.readInt()
+    val message = input match {
+      case x if x <= 0 => "Negative/Zero is input"
+      case x if x % 2 == 0 => "Even number is given"
+      case x => "Odd number is given"
+    }
+    println(message)
   }
 }
