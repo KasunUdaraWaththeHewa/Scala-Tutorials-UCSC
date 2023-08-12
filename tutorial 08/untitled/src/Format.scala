30766171619ad01661d7d015b028cee7017adb7c
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-08-12 19:21:10 +0530

    Program 3 done

--- tutorial 08/untitled/src/Format.scala
+++ tutorial 08/untitled/src/Format.scala
@@ -1,5 +1,12 @@
 object Format {
-  def main(args: Array[String]): Unit = {
+  val toUpper: String => String = input => input.toUpperCase()
+  val toLower: String => String = input => input.toLowerCase()
+  val formatNames: (String, String => String) => String = (name, formatter) => formatter(name)
 
+  def main(args: Array[String]): Unit = {
+    println(formatNames("Benny", toUpper))
+    println(formatNames("Ni", toUpper) + formatNames("roshan", toLower))
+    println(formatNames("Saman", toLower))
+    println(formatNames("K", toUpper) + formatNames("umar", toLower) + formatNames("a", toUpper))
   }
 }
